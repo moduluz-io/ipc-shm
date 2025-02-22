bd5a2103cc53779a120f6c176957b5b786ff8373
thadathilsinan <thadathilsinan@gmail.com>
2025-02-22 13:48:54 +0530

    Updated the function wrapper

--- src/fn_registry/fn.cc
+++ src/fn_registry/fn.cc
@@ -1,29 +1,20 @@
 #include <fn_registry/fn.h>
 
-template <typename Func>
-IPC::Function::Function(Func&& f) : func([f = std::forward<Func>(f)](std::vector<std::any> args) -> std::any {
-    return callWithArgs(f, std::move(args));
-    }) {
+#include <stdexcept>
+
+template <typename Ret, typename... Args>
+IPC::Function::Function(std::function<Ret(Args...)> func) {
+    /**
+     * Creates an anonymous function that takes a vector of std::any arguments
+     * and calls the function with the arguments unpacked from the vector.
+     */
+    function_ = [func](const std::vector<std::any>& args) -> std::any {
+        if (args.size() != sizeof...(Args)) throw std::runtime_error("Incorrect argument count");
+        auto tuple_args = vector_to_tuple<Args...>(args);
+        return std::apply(func, tuple_args);
+        };
 }
 
-std::any IPC::Function::operator()(std::vector<std::any> args) {
-    return func(std::move(args));
-}
-
-template <typename Func, typename... Args>
-std::any IPC::Function::callWithArgs(Func& f, std::vector<std::any>&& args) {
-    if (sizeof...(Args) == args.size()) {
-        return std::apply(f, makeTuple<Args...>(std::move(args)));
-    }
-    return {}; // Return empty std::any if argument count mismatches
-}
-
-template <typename... Args>
-std::tuple<Args...> IPC::Function::makeTuple(std::vector<std::any>&& args) {
-    return makeTupleHelper<Args...>(std::move(args), std::index_sequence_for<Args...>{});
-}
-
-template <typename... Args, size_t... Is>
-std::tuple<Args...> IPC::Function::makeTupleHelper(std::vector<std::any>&& args, std::index_sequence<Is...>) {
-    return std::make_tuple(std::any_cast<Args>(args[Is])...);
+std::any IPC::Function::invoke(const std::vector<std::any>& args) {
+    return function_(args);
 }
--- src/fn_registry/fn.h
+++ src/fn_registry/fn.h
@@ -12,28 +12,15 @@ namespace IPC {
      */
     class Function {
     public:
-        /**
-         * Creates a Function object from a lambda or a function pointer.
-         */
-        template <typename Func>
-        Function(Func&& f);
+        template <typename Ret, typename... Args>
+        Function(std::function<Ret(Args...)> func);
 
-        std::any operator()(std::vector<std::any> args);
+        /**
+         * Calls the function with the arguments unpacked from the vector.
+         */
+        std::any invoke(const std::vector<std::any>& args);
 
     private:
-        /// Represents a function that takes a vector of std::any arguments and returns a std::any
-        std::function<std::any(std::vector<std::any>)> func;
-
-        // Call function with unpacked arguments and return the result
-        template <typename Func, typename... Args>
-        static std::any callWithArgs(Func& f, std::vector<std::any>&& args);
-
-        // Convert std::vector<std::any> to a tuple of expected argument types
-        template <typename... Args>
-        static std::tuple<Args...> makeTuple(std::vector<std::any>&& args);
-
-        // Unpack the std::any into the correct types for the tuple
-        template <typename... Args, size_t... Is>
-        static std::tuple<Args...> makeTupleHelper(std::vector<std::any>&& args, std::index_sequence<Is...>);
+        std::function<std::any(const std::vector<std::any>&)> function_;
     };
 }
